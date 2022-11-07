622b57ef551d443a3c9cc7cdcc5bf6121a711126
MinsungJ <whalstjd2376@gmail.com$>
2022-11-08 02:29:20 +0900

    3.c Modified2

--- 3.c
+++ 3.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include “myProject.h"
+#include "myProject.h"
 
 int printMyName3(void) 
 {
-printf("3. my name is [Song ji hun]\n"; 
+printf("3. my name is [Song ji hun]\n"); 
 return 1;
 }
