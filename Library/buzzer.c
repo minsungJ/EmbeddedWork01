72a2690ceb649970063caa4cc07dbd03d609cdd5
Minsung Cho <whalstjd2376@gmail.com>
2022-11-29 13:42:45 +0900

    Library Final

---
+++ Library/buzzer.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <linux/input.h>
+#include <unistd.h> // for open/close
+#include <fcntl.h> // for O_RDWR
+#include <sys/ioctl.h> // for ioctl
+#include <sys/msg.h>
+#include <pthread.h>
+#include "buzzer.h"
+// first read input device
+
+#define MAX_SCALE_STEP 8
+#define BUZZER_BASE_SYS_PATH "/sys/bus/platform/devices/”
+#define BUZZER_FILENAME "peribuzzer"
+#define BUZZER_ENABLE_NAME "enable"
+#define BUZZER_FREQUENCY_NAME "frequency
+
+int buzzerInit(void)
+{
+
+}
+
+int buzzerPlaySong(int scale)
+{
+
+}
+
+int buzzerStopSong(void)
+{
+
+}
+
+int buzzerExit(void)
+{
+
+}
